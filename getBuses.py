591283be90e98ec7bf83801c251f56877550e197
Uli Hitzel <uli.hitzel@gmail.com>
2017-12-28 08:05:29 +0000

    changed

--- getBuses.py
+++ getBuses.py
@@ -4,4 +4,41 @@ from StringIO import StringIO
 
 endpoint="http://datamall2.mytransport.sg/ltaodataservice/BusServices"
 
-# source apikey.cfg
+execfile("apikey.cfg")
+
+# print "Using AccountKey " +str(AccountKey)
+
+# Get the first batch
+buffer = StringIO()
+c = pycurl.Curl()
+c.setopt(pycurl.HTTPHEADER, ['AccountKey: ' + AccountKey])
+c.setopt(c.URL, endpoint)
+c.setopt(c.WRITEDATA, buffer)
+c.perform()
+c.close()
+
+body = buffer.getvalue()
+data = json.loads(body)
+
+services1 = data['value']
+
+# Get the second batch
+buffer2 = StringIO()
+c = pycurl.Curl()
+c.setopt(pycurl.HTTPHEADER, ['AccountKey: ' + AccountKey])
+c.setopt(c.URL, endpoint + "/?$skip=500")
+c.setopt(c.WRITEDATA, buffer2)
+c.perform()
+c.close()
+
+body = buffer2.getvalue()
+data = json.loads(body)
+
+services2 = data['value']
+
+# Concat both lists
+services = services1 + services2
+
+for service in services:
+	print "Bus Service " + str(service['ServiceNo'])
+
