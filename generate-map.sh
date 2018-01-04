a3957bdb8174f74187770a9e9def30cb84f79df8
Uli Hitzel <uli.hitzel@gmail.com>
2018-01-04 16:21:20 +0800

    changed

--- generate-map.sh
+++ generate-map.sh
@@ -1,11 +1,17 @@
-echo Generating inserts...
+echo Getting bus stop loads...
+./getStopsWithLoad.sh
+
+echo Generating inserts...ignoring bus stops where all buses look ok
 python generate_MapInserts.py | grep -v busgreen > inserts.inc
 echo Concatenating...
+
+ts=$(export TZ="Asia/Singapore";date +%Y%m%d-%H%M)
+out="out/sg-$ts.html"
 
 # for this to work please create a file called googlemaps-key.inc and copy paste your Google Maps key into it. echo $key > googlemaps-key.inc
 
 google=$(cat googlemaps-key.inc)
 
-cp map-1.html out.html
-cat inserts.inc >> out.html
-cat map-2.html | sed "s/GOOGLE_KEY/$google/;" >> out.html
+cp map-1.html $out
+cat inserts.inc >> $out
+cat map-2.html | sed "s/GOOGLE_KEY/$google/;" >> $out
