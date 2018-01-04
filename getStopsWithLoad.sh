0661b681edcc34b7b126e50e47128ac1ad3b6c59
Uli Hitzel <uli.hitzel@gmail.com>
2018-01-04 19:46:32 +0800

    changed

--- getStopsWithLoad.sh
+++ getStopsWithLoad.sh
@@ -2,4 +2,4 @@ ts=$(export TZ="Asia/Singapore";date +%Y%m%d-%H%M)
 
 python getBusyBusStops.py > busStopsWithLoads.cfg
 
-cp busStopsWithLoads.cfg data/busStopsWithLoads-$ts.cfg
+cp busStopsWithLoads.cfg data/busStopsWithLoads-$ts.dat
