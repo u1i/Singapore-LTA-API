24af18ec1e03f8da70be182eaa4574d92ad6ec34
Uli Hitzel <uli.hitzel@gmail.com>
2018-01-05 11:41:44 +0800

    changed

--- generate-index.sh
+++ generate-index.sh
@@ -1,11 +1,11 @@
 start=$PWD
 
-cd $start/out
-rm index.html
-for d in sg*html
-do
-	echo "<a href='$d' target='_new'>$d</a><br>" >> index.html
-done
+# cd $start/out
+# rm index.html
+# for d in sg*html
+# do
+# 	echo "<a href='$d' target='_new'>$d</a><br>" >> index.html
+# done
 
 cd $start/maps
 
