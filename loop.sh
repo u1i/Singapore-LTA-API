d7b4a869c74502b8134b0a95d4f48e1a4f5f1b9c
Uli Hitzel <uli.hitzel@gmail.com>
2018-01-05 17:20:18 +0800

    changed

--- loop.sh
+++ loop.sh
@@ -1,9 +1,19 @@
+while [ 1 ]
+do
+	date
 
-hour=$(export TZ="Asia/Singapore";date +%H)
+	hour=$(export TZ="Asia/Singapore";date +%H)
+	
+	echo $hour
+	
+	case $hour in
+	    06|07|08|09|10|11|12|13|14|15|16|17|18|19) run="yes";;
+	    *)             run="no";;
+	esac
 
-echo $hour
+	echo "Run: $run"
 
-case $hour in
-    06|07|08|09|10|11|12|13|14|15|16|17|18|19) echo yes;;
-    *)             echo no;;
-esac
+	./generate-map.sh
+	./push.sh
+	sleep 300
+done
