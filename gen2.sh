cee01db47ebb99d5756ca4a45c90b529bcd83e8d
Uli Hitzel <uli.hitzel@gmail.com>
2018-01-05 11:20:03 +0800

    changed

--- gen2.sh
+++ gen2.sh
@@ -1,2 +1,5 @@
-ts=$(export TZ="Asia/Singapore";date +%Y%m%d-%H%M)
-out="maps/sg-$ts.html"
+ts=$(export TZ="Asia/Singapore";date +%Y-%m-%d/%H%M)
+out="maps/$ts.html"
+dir=$(dirname $out)
+mkdir -p $dir 2>/dev/null
+echo $out in $dir
