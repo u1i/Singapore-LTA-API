caac8963f80d4c9f655933586646f19f2c24de89
Uli Hitzel <uli.hitzel@gmail.com>
2018-01-05 03:30:04 +0000

    changed

--- generate-index.sh
+++ generate-index.sh
@@ -13,5 +13,13 @@ rm index.html
 for d in $(ls)
 do
 	echo "<a href='$d'>$d</a><br>" >> index.html
+	cd $start/maps/$d
+
+	rm index.html 2>/dev/null
+
+	for d in *html
+	do
+		echo "<a href='$d' target='_new'>$d</a><br>" >> index.html
+	done
 done
 
