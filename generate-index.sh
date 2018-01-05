68e30fa17b736c2b5c97a37a527738dc72734562
Uli Hitzel <uli.hitzel@gmail.com>
2018-01-05 11:25:27 +0800

    changed

--- generate-index.sh
+++ generate-index.sh
@@ -1,6 +1,17 @@
-cd out
+start=$PWD
+
+cd $start/out
 rm index.html
 for d in sg*html
 do
 	echo "<a href='$d' target='_new'>$d</a><br>" >> index.html
 done
+
+cd $start/maps
+
+rm index.html
+for d in $(find . | grep html)
+do
+	echo "<a href='$d' target='_new'>$d</a><br>" >> index.html
+done
+
--- generate-map.sh
+++ generate-map.sh
@@ -5,8 +5,13 @@ echo Generating inserts...ignoring bus stops where all buses look ok
 python generate_MapInserts.py | grep -v busgreen > inserts.inc
 echo Concatenating...
 
-ts=$(export TZ="Asia/Singapore";date +%Y%m%d-%H%M)
-out="out/sg-$ts.html"
+#ts=$(export TZ="Asia/Singapore";date +%Y%m%d-%H%M)
+#out="out/sg-$ts.html"
+
+ts=$(export TZ="Asia/Singapore";date +%Y-%m-%d/%H%M)
+out="maps/$ts.html"
+dir=$(dirname $out)
+mkdir -p $dir 2>/dev/null
 
 # for this to work please create a file called googlemaps-key.inc and copy paste your Google Maps key into it. echo $key > googlemaps-key.inc
 
